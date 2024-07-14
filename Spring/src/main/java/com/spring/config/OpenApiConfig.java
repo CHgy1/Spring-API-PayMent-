627727fe6b62c3b51cfa4be4442816b7570ce1fc
EotGGa <qkrckdrb4106@naver.com>
2024-07-14 22:31:01 +0900

    Img upload

--- Spring/src/main/java/com/spring/config/OpenApiConfig.java
+++ Spring/src/main/java/com/spring/config/OpenApiConfig.java
@@ -1,32 +1,23 @@
 package com.spring.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI springShopOpenAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("Spring Shop API")
-                .description("Spring shop sample application")
-                .version("v0.0.1")
-                .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .externalDocs(new ExternalDocumentation()
-                .description("SpringShop Wiki Documentation")
-                .url("https://springshop.wiki.github.org/docs"))
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                            new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
-                    .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"))
+                .info(new Info().title("API").version("v1").description("API Documentation"));
     }
 }
